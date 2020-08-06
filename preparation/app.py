256c2507cef8781f9faec190a061b4dcfe244168
Akane Yamamoto <3lines.athome@gmail.com>
2020-08-06 13:22:27 +0900

    flask

---
+++ preparation/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def helloWorld():
+    return "Hello World."
+
+@app.route("/<name>")
+def greet(name):
+    return name + "さん、こんにちは"
+
+@app.route("/template")
+def template():
+    py_name = "サワガニ"
+    return render_template("index.html", name = py_name)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
